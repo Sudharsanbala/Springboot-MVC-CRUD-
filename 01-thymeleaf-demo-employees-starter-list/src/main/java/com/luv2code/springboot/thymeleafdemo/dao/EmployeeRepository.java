56612c264dc7f2d89e094c29f96743658a2a16e9
Sudharsanbala <sudharsanbalasubramian390@gmail.com>
2023-06-20 18:16:03 +0530

    commit my code

---
+++ 01-thymeleaf-demo-employees-starter-list/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -0,0 +1,19 @@
+package com.luv2code.springboot.thymeleafdemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+	// that's it ... no need to write any code LOL!
+
+    //add a method sort by lastname
+    public List<Employee> findAllByOrderByLastNameAsc();
+
+    //findAllByOrderByLastNameAsc Spring Data jpa will parse this method name and looks for specific format and pattern
+    //and create appropriate query behind the scenes
+	
+}
